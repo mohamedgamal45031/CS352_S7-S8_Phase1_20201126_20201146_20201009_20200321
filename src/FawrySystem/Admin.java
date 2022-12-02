f11aab85ae32a7f12c771c39da88082aec331bf0
A7madAdelll <ahmadaa145632@gmail.com>
2022-12-02 19:43:45 +0200

    admin class has been added

---
+++ src/FawrySystem/Admin.java
@@ -0,0 +1,24 @@
+package FawrySystem;
+
+public class Admin {
+
+    /*-
+- email: string
+- password: string
+*/
+    private String email;
+    private String password;
+
+
+    public void setDiscount(String s,double discount){
+        Search search=Search.getInstance();
+        search.putService(s,new Discount(discount,search.getService(s)));
+    }
+    public void setDiscountForAllServices(double discount){
+        Search search=Search.getInstance();
+        search.setDiscountOverAllServices(discount);
+    }
+
+
+
+}
